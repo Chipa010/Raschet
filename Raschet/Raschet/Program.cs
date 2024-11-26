b0a2b2ebc617c46424a39f194671b138748320dc
Chipa010 <porovozova14@gmail.com>
2024-11-26 21:06:55 +0900

    Raschet

---
+++ Raschet/Raschet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class AbsoluteDifference
+{
+    public static int CalculateDifference(int n)
+    {
+        int difference = Math.Abs(n - 123); // Абсолютная разность
+        return n > 123 ? difference * 3 : difference; // Тройная разность, если n > 123
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите число n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = CalculateDifference(n);
+        Console.WriteLine($"Результат: {result}");
+    }
+}
